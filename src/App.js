a165bcfb5365a424b92a58552ebb613dfe96676f
Barış Belevi <belevibarisofficial@gmail.com>
2024-09-23 19:30:32 +0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import "./styles.css";
+import Spline from "@splinetool/react-spline";
+
+export default function App() {
+  return (
+    <div className="spline-container">
+      <Spline scene="https://prod.spline.design/AxoG0oQwdwVcy37C/scene.splinecode" />
+    </div>
+  );
+}
